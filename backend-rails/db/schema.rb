1944ea9403f883373a468e8285b5b4db10db0a59
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-05-18 04:58:25 -0600

    Add authors and books

--- backend-rails/db/schema.rb
+++ backend-rails/db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_034707) do
+ActiveRecord::Schema.define(version: 2020_05_17_235017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
 
   create_table "branches", force: :cascade do |t|
     t.string "name"
@@ -32,4 +47,5 @@ ActiveRecord::Schema.define(version: 2019_10_18_034707) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "books", "authors"
 end
