76e079aad8aa2e2c45fad7eef45d800afeedcfbf
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-05-19 07:32:23 -0600

    allow all http methods in CORS policy

--- backend-rails/config/environments/production.rb
+++ backend-rails/config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'lms.blainelafreniere.io'
-      resource '*', headers: :any, methods: [:get, :post, :options]
+      resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
     end
   end
 
