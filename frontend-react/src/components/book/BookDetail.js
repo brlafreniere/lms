0098588318c1aab40e2fc2f11734d2bb4902b3ba
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-06-02 18:33:34 -0600

    Add OverlayMenu to BookDetail (WIP)

--- frontend-react/src/components/book/BookDetail.js
+++ frontend-react/src/components/book/BookDetail.js
@@ -2,10 +2,12 @@ import React from "react";
 import Axios from "axios";
 import {
     useParams,
-    Link,
-    Switch,
-    Route
 } from "react-router-dom";
+
+import Book from "../../modules/book";
+import OverlayMenu from "../OverlayMenu"
+
+import "./BookDetail.css"
 
 export default function BookDetail(props) {
     let { id } = useParams();
@@ -18,7 +20,8 @@ class BookDetailComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            book: {}
+            book: {},
+            showCheckoutMenu: false
         }
     }
 
@@ -27,24 +30,57 @@ class BookDetailComponent extends React.Component {
     }
 
     loadBook = () => {
-        Axios.get(`${process.env.REACT_APP_API_URL}/books/${this.props.bookId}`).then(response => {
-            this.setState({book: response.data})
+        Book.fetch(this.props.bookId).then(book => {
+            this.setState({book})
         }).catch(console.log)
+    }
+
+    showCheckoutMenu = () => {
+        this.setState({showCheckoutMenu: true})
+    }
+
+    CheckoutMenu = (props) => {
+        if (this.state.showCheckoutMenu) {
+            return (
+                <OverlayMenu closeOverlay={e => this.setState({showCheckoutMenu: false})}>
+                    Ooga McBooga
+                </OverlayMenu>
+            )
+        } else {
+            return null;
+        }
+    }
+
+    CoverImage = () => {
+        return (
+            <div className='book-detail-image'>
+                <div>
+                    <img src={`${process.env.REACT_APP_API_URL}/uploads/${this.state.book.cover_image_file_name}`}
+                        width="200"
+                        alt={this.state.book.title + 'cover image'}/>
+                </div>
+            </div>
+        )
+    }
+
+    TitleAndSynopsis = () => {
+        return (
+            <div className='book-detail-title-and-synopsis'>
+                <h2>{this.state.book.title}</h2>
+                {this.state.book.synopsis}
+            </div>
+        )
     }
 
     render() {
         return (
             <div className='book-detail'>
-                <div className='book-detail-image'>
-                    <div><img src={`${process.env.REACT_APP_API_URL}/uploads/${this.state.book.cover_image_file_name}`} width="200" alt={this.state.book.title + 'cover image'}/></div>
+                <this.CoverImage />
+                <this.TitleAndSynopsis />
+                <div className='book-detail-options mt-3'>
+                    <button className="btn btn-primary" onClick={this.showCheckoutMenu}>Check Out</button>
                 </div>
-                <div className='book-detail-title-and-synopsis'>
-                    <h2>{this.state.book.title}</h2>
-                    {this.state.book.synopsis}
-                    <div className='book-detail-options'>
-                        <Link to={`/books/${this.state.book.id}/checkout`} className="btn btn-primary">Check Out</Link>
-                    </div>
-                </div>
+                <this.CheckoutMenu />
             </div>
         )
     }
