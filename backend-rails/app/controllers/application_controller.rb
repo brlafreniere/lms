af6d3cb165dfd86ac4fe56f6108950d0bdde9314
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-06-03 23:58:26 -0600

    check for auth header on every request

--- backend-rails/app/controllers/application_controller.rb
+++ backend-rails/app/controllers/application_controller.rb
@@ -1,17 +1,33 @@
 require "#{Rails.root}/lib/jwt_transcoder"
 
 class ApplicationController < ActionController::API
-  def authenticate_request
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    begin
-      @decoded = JWTTranscoder.decode(header)
-      @current_user = User.find(@decoded[:user_id])
-      Current.user = @current_user
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { errors: e.message }, status: :unauthorized
+    before_action :set_current_user
+
+    def authenticate_request
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = JWTTranscoder.decode(header)
+            @current_user = User.find(@decoded[:user_id])
+            Current.user = @current_user
+        rescue ActiveRecord::RecordNotFound => e
+            render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            render json: { errors: e.message }, status: :unauthorized
+        end
     end
-  end
+
+    def set_current_user
+        header = request.headers['Authorization']
+        header = header.split(' ').last if header
+        begin
+            @decoded = JWTTranscoder.decode(header)
+            @current_user = User.find(@decoded[:user_id])
+            Current.user = @current_user
+        rescue ActiveRecord::RecordNotFound => e
+            # render json: { errors: e.message }, status: :unauthorized
+        rescue JWT::DecodeError => e
+            # render json: { errors: e.message }, status: :unauthorized
+        end
+    end
 end
