885020f0f992676f4961a5314943c6056795dbf4
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-06-04 00:03:11 -0600

    Create show action for users, for my-account page

--- backend-rails/app/controllers/users_controller.rb
+++ backend-rails/app/controllers/users_controller.rb
@@ -1,21 +1,31 @@
 class UsersController < ApplicationController
-  def authenticate
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      token = JWTTranscoder.encode(user_id: user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, expiration: time.strftime("%m-%d-%Y %H:%M"), user: user.as_json(except: :password_digest)}, status: :ok
-    else
-      render json: { error: 'Unauthorized' }, status: :unauthorized
+    def authenticate
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            token = JWTTranscoder.encode(user_id: user.id)
+            time = Time.now + 24.hours.to_i
+            render json: { token: token, expiration: time.strftime("%m-%d-%Y %H:%M"), user: user.as_json(except: :password_digest)}, status: :ok
+        else
+            render json: { error: 'Unauthorized' }, status: :unauthorized
+        end
     end
-  end
 
-  def create
-    @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if @user.save
-        render status: :created
-    else
-        render status: 401
+    def show
+        # if params[:id] != Current.user
+        #     # naughty person trying to do naughty things
+        #     render status: :unauthorized
+        # end
+
+        @user = User.find(params[:id])
+        render json: @user.to_json
     end
-  end
+
+    def create
+        @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+        if @user.save
+            render status: :created
+        else
+            render status: 401
+        end
+    end
 end
