4672c2cec095286078a0648f8e380f41cd3c4ff3
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-06-09 03:31:49 -0600

    Remove useless config option, it is on by default

--- backend-rails/config/environments/development.rb
+++ backend-rails/config/environments/development.rb
@@ -6,8 +6,6 @@ Rails.application.configure do
     end
   end
 
-  config.public_file_server.enabled = true
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
