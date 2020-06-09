f66ac8928f2522ca5299f062ee522c1d143810ee
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-06-09 04:42:20 -0600

    Destroys associated records when branch is deleted

--- backend-rails/app/models/branch.rb
+++ backend-rails/app/models/branch.rb
@@ -1,4 +1,14 @@
 class Branch < ApplicationRecord
-    has_many :book_inventories
+    # before_destroy :delete_inventory
+
+    has_many :book_inventories, dependent: :destroy
+    has_many :reservations, dependent: :destroy
+    has_many :checkouts, dependent: :destroy
     has_many :books, through: :book_inventories
+
+    # private
+    # def delete_inventory
+    #     puts "here"
+    #     self.book_inventories.delete_all
+    # end
 end
