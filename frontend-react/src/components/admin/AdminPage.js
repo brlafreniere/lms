1688047facbfcda133dfd9fe38f8bb871d54a41c
Blaine Lafreniere <brlafreniere@gmail.com>
2020-06-11 00:16:40 -0600

    Modularize code a tiny bit for Admin Page

--- frontend-react/src/components/admin/AdminPage.js
+++ frontend-react/src/components/admin/AdminPage.js
@@ -19,37 +19,69 @@ import BookForm from "../../components/book/BookForm";
 import BookDetailAdmin from "./../book/BookDetailAdmin";
 import BookInventoryFormWithID from "./../book/BookInventory";
 
+function AdminNavigationTabs() {
+    return (
+        <nav>
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <NavLink to="/admin/books" className="nav-link" activeClassName="active">Books</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/admin/authors" className="nav-link" activeClassName="active">Authors</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/admin/branches" className="nav-link" activeClassName="active">Branches</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/admin/members" className="nav-link" activeClassName="active">Members</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/admin/goodreads" className="nav-link" activeClassName="active">Goodreads</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+function AdminNavigationSwitch() {
+    return (
+        <Switch>
+            <Route path="/admin/books">
+                <RecordManager
+                    key="book"
+                    singular="book"
+                    plural="books"
+                    table={BookTable}
+                    form={BookForm}
+                    detail={BookDetailAdmin}
+                    inventory={BookInventoryFormWithID} />
+            </Route>
+            <Route path="/admin/authors">
+                <RecordManager
+                    key="author"
+                    singular="author"
+                    plural="authors"
+                    table={AuthorTable}
+                    form={AuthorForm} />
+            </Route>
+            <Route path="/admin/branches">
+                <RecordManager 
+                    key="branch"
+                    singular="branch"
+                    plural="branches"
+                    table={BranchTable}
+                    form={BranchForm} />
+            </Route>
+        </Switch>
+    )
+}
+
 export default class AdminPage extends React.Component {
     render() {
         return (
             <div>
-                <nav>
-                    <ul className="nav nav-tabs">
-                        <li className="nav-item">
-                            <NavLink to="/admin/books" className="nav-link" activeClassName="active">Books</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/admin/authors" className="nav-link" activeClassName="active">Authors</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/admin/branches" className="nav-link" activeClassName="active">Branches</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/admin/members" className="nav-link" activeClassName="active">Members</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path="/admin/books">
-                        <RecordManager key="book" singular="book" plural="books" table={BookTable} form={BookForm} detail={BookDetailAdmin} inventory={BookInventoryFormWithID} />
-                    </Route>
-                    <Route path="/admin/authors">
-                        <RecordManager key="author" singular="author" plural="authors" table={AuthorTable} form={AuthorForm} />
-                    </Route>
-                    <Route path="/admin/branches">
-                        <RecordManager key="branch" singular="branch" plural="branches" table={BranchTable} form={BranchForm} />
-                    </Route>
-                </Switch>
+                <AdminNavigationTabs />
+                <AdminNavigationSwitch />
             </div>
         )
     }
